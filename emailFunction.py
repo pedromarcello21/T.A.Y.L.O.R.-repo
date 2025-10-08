7a2173dc1b648ad962a7e12055ba5bb89f4ad497
pedromarcello <vincentypedro@gmail.com>
2025-10-08 17:07:32 -0400

    add functionality

---
+++ emailFunction.py
@@ -0,0 +1,42 @@
+import os
+from email.message import EmailMessage
+#layer of security for keeping information secure
+import ssl
+import smtplib
+import datetime as dt
+today = dt.datetime.today().strftime('%m/%d/%Y')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def send_email(body):
+    """Send introductory email"""
+    email_sender="vincentypedro@gmail.com"
+    email_password = os.environ.get("EMAIL_PASSWORD")
+    email_receiver = "vincentypedro@gmail.com"
+
+    
+
+    em = EmailMessage()
+
+    em['From'] = "Pedro Vincenty"
+    em['To'] = email_receiver
+    em['Subject'] = f"Love Letter for Taylor | {today}"
+
+    em.set_content(body)
+        
+    
+    context = ssl.create_default_context()
+
+
+    try:
+        #from stack overflow
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.send_message(em)
+            return "Sent!"
+    except:
+        return "error sending email"
+    
+
+    
---
+++ mommyNewsletter.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import openai
+import json
+import os
+#layer of security for keeping information secure
+import ssl
+import smtplib
+import datetime as dt
+import random
+from emailFunction import send_email
+
+
+load_dotenv()  # this reads the .env file
+
+openai.api_key = os.getenv('API_KEY')
+
+prompts = "Tell Taylor how much I love her and how beautiful of a girlfriend she is.  Include emojis."
+
+prompt = random.choice(prompts)
+completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}],
+)
+
+body = completion.choices[0].message
+
+print(body.content.strip()+ "\n\nI love you")
+
+send_email(body.content.strip()+ "\n\nI love you")
